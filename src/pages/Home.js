95ca1e46082df28cbf562b9e54be4bd50f315a30
Shady Ibrahim <shadyaziz72@gmail.com>
2025-04-04 15:50:51 -0400

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -85,7 +85,7 @@ export default function Home() {
   return (
     <div className='mq-home'>
       <Header
-        title='Arcane Quests'
+        title='Wizard Land'
         suppressLevel
       />
       <main className='mq-dashboard-cards-wrapper'>
