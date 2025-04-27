67bd8e8cb817df8ff5972165c0ad4f32799160e9
Shady Ibrahim <shadyaziz72@gmail.com>
2025-04-27 08:54:36 -0400

    update look of buttons

--- src/components/Header/UserStats.js
+++ src/components/Header/UserStats.js
@@ -3,9 +3,13 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import expIcon from '../../assets/images/elements/exp.png';
 import euroIcon from '../../assets/images/elements/euro.png';
 import starIcon from '../../assets/images/elements/star.png';
+import emailIcon from '../../assets/images/elements/email.png';
+import supportIcon from '../../assets/images/elements/support.png';
+
 import Button from '../Button';
 import SendEmailModal from '../authModals/sendEmail';
 import { useState } from 'react';
+import { playButtonHover } from '../../hooks/useSound';
 
 const UserStats = ({ loading, exp, euro, stars, userId }) => {
   const [showEmailModal, setShowEmailModal] = useState(false);
@@ -69,19 +73,27 @@ const UserStats = ({ loading, exp, euro, stars, userId }) => {
             </span>
           </span>
           <span className='mq-user-progress--contact'>
+            <span className='mq-user-icon'>
+              <img src={emailIcon} />
+            </span>
             <a
               type='button'
               id='send-email'
               className='icon-button'
               onClick={() => setShowEmailModal(true)}
+              onMouseEnter={playButtonHover}
             >
               Feedback
             </a>
           </span>
           <span className='mq-user-progress--contact'>
+            <span className='mq-user-icon'>
+              <img src={supportIcon} />
+            </span>
             <a
               href='https://www.buymeacoffee.com/wizardland'
               target='_blank'
+              onMouseEnter={playButtonHover}
             >
               Support My Work
             </a>
