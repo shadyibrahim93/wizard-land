77716a4f6a2c99d6a10bd36bfc819fe923f2ac69
Shady Ibrahim <shadyaziz72@gmail.com>
2025-05-09 14:13:23 -0400

    Updates

--- static/js/952.406e2d23.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkhubspot_project=self.webpackChunkhubspot_project||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.406e2d23.chunk.js.map
