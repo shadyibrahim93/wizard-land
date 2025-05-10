407517cefe702111585eea48231aefcfa6f08a0c
Shady Ibrahim <shadyaziz72@gmail.com>
2025-05-10 10:23:11 -0400

    remove comments and update sitemap.xml

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const customDomain = 'wizardland.net'; // 🚨 Replace with your actual domain
+const customDomain = 'wizardland.net';
 
 const nextConfig = {
   images: {
     formats: ['image/webp'],
-    unoptimized: true // Disables the built-in Image Optimization API
+    unoptimized: true
   },
   output: 'export',
   distDir: 'build',
   trailingSlash: true,
   webpack: (config, { isServer }) => {
-    // Emit .webp files into .next/static/media with a content hash
     config.module.rules.push({
       test: /\.webp$/i,
       type: 'asset/resource',
