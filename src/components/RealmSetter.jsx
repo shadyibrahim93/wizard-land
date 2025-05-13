03a359f120ef5a8cb9add44c51228455a063bf80
Shady Ibrahim <shadyaziz72@gmail.com>
2025-05-13 07:23:22 -0400

    fix showing the correct realm on load

--- src/components/RealmSetter.jsx
+++ src/components/RealmSetter.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useUser } from '../context/UserContext.js';
 import { useSelectedPiece } from '../hooks/userSelectedPiece.js';
 
@@ -9,16 +9,21 @@ export default function RealmSetter() {
 
   useEffect(() => {
     if (!player) return; // Wait until player data is available
-
     const localStorageRealm = localStorage.getItem('realm');
-    const playerRealm = localStorageRealm || player?.realm || 'fantasy';
+    const defaultRealm = player?.realm || 'fantasy';
+    const playerRealm = localStorageRealm || player?.realm || defaultRealm;
 
     // Update DOM and storage
     document.documentElement.setAttribute('data-theme', playerRealm);
-    localStorage.setItem('realm', playerRealm);
 
     console.log('Realm initialized:', playerRealm);
   }, [player]); // Only re-run when player data changes
+
+  useEffect(() => {
+    if (player?.realm) {
+      localStorage.setItem('realm', player.realm);
+    }
+  }, [player]);
 
   return null;
 }
--- src/hooks/userSelectedPiece.js
+++ src/hooks/userSelectedPiece.js
@@ -4,19 +4,18 @@ import useSelectedItems from './useSelectedItems';
 
 export const useSelectedPiece = (userId, fallback, themefallback) => {
   const selected = useSelectedItems(userId);
-  const [localStorageRealm, setLocalStorageRealm] = useState('fantasy');
-
-  useEffect(() => {
-    // This effect only runs on the client side
-    const realm = window.localStorage.getItem('realm');
-    setLocalStorageRealm(realm);
-  }, []);
 
   const emoji = selected.piece?.emoji || null;
   const image = selected.piece?.image_url || null;
   const name = selected.piece?.className || null;
   const theme = selected.theme?.className || null;
-  const realm = localStorageRealm || selected.realm?.className;
+  const realm = selected.realm?.className;
+
+  useEffect(() => {
+    if (realm) {
+      window.localStorage.setItem('realm', realm);
+    }
+  }, [realm]);
 
   return {
     key: name || emoji || fallback, // used for comparison
