85b4ef969387c51c16cd18d5b4a036c7ecd100ff
Shady Ibrahim <shadyaziz72@gmail.com>
2025-05-24 11:45:25 -0400

    add avatar and presence checker

--- src/components/chatRoom.jsx
+++ src/components/chatRoom.jsx
@@ -10,9 +10,11 @@ import Button from './Button.js';
 import { playUncover, playDisappear } from '../hooks/useSound.js';
 import { useUser } from '../context/UserContext.js';
 import useSelectedRealm from '../hooks/userSelectedRealm.js';
+import Avatar from 'boring-avatars';
+import UserOnlineStatus from './UserOnlineStatus.jsx';
 
 const GameChat = ({ chatTitle, gameId }) => {
-  const { userId, userName } = useUser();
+  const { userId, userName, isOnline } = useUser();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
   const { realm, resolved } = useSelectedRealm();
@@ -22,6 +24,8 @@ const GameChat = ({ chatTitle, gameId }) => {
       const fetchedMessages = await fetchMessages(gameId);
       setMessages(fetchedMessages);
     };
+
+    console.log(isOnline);
 
     fetchMessagesForRoom();
 
@@ -68,25 +72,46 @@ const GameChat = ({ chatTitle, gameId }) => {
         </header>
         <div className='mq-messages'>
           {messages.length === 0 && <h3>Start Chating!</h3>}
+
           {messages.map((msg) => (
-            <div
-              className={`${
-                msg.sender_name === userName ? 'mq-local-user' : ''
-              }`}
-            >
-              <span className='mq-message--user'>
-                {msg.sender_name === userName ? 'You' : msg.sender_name}
-              </span>
+            <>
               <div
-                key={msg.id ?? Math.random()}
-                className='mq-message'
+                key={msg.id ?? `${msg.sender_name}-${Math.random()}`}
+                className={`${
+                  msg.sender_name === userName
+                    ? 'mq-user-container mq-local-user'
+                    : 'mq-user-container'
+                }`}
               >
-                <span className='mq-message--messsage'>{msg.message}</span>
-                <span className='mq-message--date'>
-                  {new Date(msg.created_at).toLocaleString()}
-                </span>
+                <div
+                  key={msg.id ?? Math.random()}
+                  className='mq-message'
+                >
+                  <span className='mq-message--user'>
+                    {msg.sender_name === userName ? 'You' : msg.sender_name}
+                  </span>
+                  <span className='mq-message--messsage'>{msg.message}</span>
+                </div>
+                <div className='mq-player-profile'>
+                  <Avatar
+                    size={30}
+                    name={msg.sender_name && msg.sender_name}
+                    variant='beam'
+                    colors={[
+                      '#92A1C6',
+                      '#146A7C',
+                      '#F0AB3D',
+                      '#C271B4',
+                      '#C20D90'
+                    ]}
+                  />
+                  {msg.sender_id && <UserOnlineStatus userId={msg.sender_id} />}
+                </div>
               </div>
-            </div>
+              <span className='mq-message--date'>
+                {new Date(msg.created_at).toLocaleString()}
+              </span>
+            </>
           ))}
         </div>
       </div>
