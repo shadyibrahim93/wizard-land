bf06427ab6cd3a484d0847d84d89ae26eb5f50fe
Shady Ibrahim <shadyaziz72@gmail.com>
2025-05-26 12:58:35 -0400

    Updates

---
+++ _next/static/chunks/app/privacy-policy/page-3b19b20e5f01081b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2902,9222],{8161:(e,s,n)=>{Promise.resolve().then(n.bind(n,4566)),Promise.resolve().then(n.bind(n,3113))}},e=>{var s=s=>e(e.s=s);e.O(0,[445,6711,7753,7672,8429,844,3561,4212,2940,6719,2962,8441,1684,7358],()=>s(8161)),_N_E=e.O()}]);
